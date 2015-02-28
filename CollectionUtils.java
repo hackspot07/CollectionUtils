eee04ed02fe93b76cc53262f0059ee46c7c2f854
parmatma <pyadav.it@gmail.com>
2015-02-28 15:58:55 +0530

    vrsion 1 generic

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
-interface OperateAble{
-	int operate(int ele);
-}
-
 public class CollectionUtils{
-	public CollectionUtils(){
-	
+	public static <E> List<E> map(List<E> list,OperateAble op){
+		List<E> newlist = new ArrayList<E>();
+		for(E i: list){
+			newlist.add((E)(op.operate(i)));
+		}
+		return newlist;
 	}
 
-	public static List<Integer> map(List<Integer> list,OperateAble op){
-		List<Integer> newlist = new ArrayList<Integer>();
-		for(int i: list){
-			newlist.add(op.operate(i));
-			System.out.println(op.operate(i));
+	public static <E> List<E> filter(List<E> list,OperateAble op){
+		List<E> newlist = new ArrayList<E>();
+		for(E i: list){
+			newlist.add((E)(op.predicate(i)));
 		}
 		return newlist;
 	}
--- CollectionUtilsTest.java
+++ CollectionUtilsTest.java
@@ -2,25 +2,47 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+interface OperateAble <E>{
+	public E operate(E ele);
+}
 
-class ListMapper implements OperateAble{
-	public ListMapper(){
-	}
-	public int operate(int ele){
+interface PredicateAble <E>{
+	public E predicate(E ele);
+}
+
+class ListMapper implements OperateAble <Integer>{
+	public  Integer operate(Integer ele){
 		return ele*2;
+	}
+};
+
+class ListFilter implements PredicateAble <Integer>{
+	public  Integer predicate(Integer ele){
+		if(ele>2)
+			return ele;
 	}
 };
 
 public class CollectionUtilsTest{
 	@Test
-	public void CollectionUtilsRturnAlist(){
+	public void CollectionUtilsListMApperRturnAlist(){
 		List<Integer> list = new ArrayList<Integer>();
-		OperateAble op = new ListMapper(); 
+		OperateAble<Integer> op = new ListMapper(); 
 		for(int i=0; i<5; i++){
 			list.add(i);
 		}
-		List<Integer> newlist = CollectionUtils .map(list,op);
+		List<Integer> newlist = CollectionUtils.<Integer>map(list,op);
 		assertEquals(newlist.size(),5);
 	}
 
+	@Test
+	public void CollectionUtilsListFilterRturnAlist(){
+		List<Integer> list = new ArrayList<Integer>();
+		PredicateAble<Integer> op = new ListFilter(); 
+		for(int i=0; i<5; i++){
+			list.add(i);
+		}
+		List<Integer> newlist = CollectionUtils.<Integer>filter(list,op);
+		assertEquals(newlist.size(),2);
+	}
 } 	
