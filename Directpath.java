59234018600d7cc3a003940fce537ffb58a20446
parmatma <pyadav.it@gmail.com>
2015-03-02 21:56:39 +0530

    version first done using list reformed

--- Directpath.java
+++ Directpath.java
@@ -1,72 +1,47 @@
 import java.util.*;
 import java.io.*;
 
-class Source {
-	private String name;
-	Source(String name) {
-		this.name = name;
-	}
-
-	public String toString(){
-		return name;
-	}
-	public boolean equals(Object other) {
-		return name.equals(((Source) other).name);
-	}
-}
-
-class Destination {
-	private String name;
-	Destination(String name) {
-		this.name = name;
-	}
-	public String toString(){
-		return name;
-	}
-}
 class Database{
-	static Map<Source, Destination> db = new HashMap<Source,Destination>();
-
-	public static Map<Source, Destination> createDbDatabase(){
-		db.put(new Source("Bangalore"), new Destination("Singapore"));
-		db.put(new Source("Singapore"), new Destination("Seoul"));
-		db.put(new Source("Singapore"), new Destination("Dubai"));
-		db.put(new Source("Seoul"), new Destination("Beijing"));
-		db.put(new Source("Beijing"), new Destination("Tokyo"));	
+	static Map<String, List<String>> db = new HashMap<String,List<String>>();
+	public static Map<String, List<String>> createDbDatabase(){
+		List<String> bangalore = new ArrayList<String>();
+		bangalore.add("Singapore");
+		db.put("Bangalore",bangalore);
+		List<String> singapore = new ArrayList<String>();
+		singapore.add("Seoul");
+		singapore.add("Dubai");
+		db.put("Singapore",singapore);
+		List<String> seoul = new ArrayList<String>();
+		seoul.add("Beijing");
+		db.put("Seoul",seoul);
+		List<String> beijing = new ArrayList<String>();
+		beijing.add("Tokyo");
+		db.put("Beijing",beijing);	
 		return db;
 	}
 }
 
 public class Directpath{
-	static Map<Source, Destination> db = Database.createDbDatabase();
+	static Map<String, List<String>> db = Database.createDbDatabase();
 	public static boolean path(String source,String destination)throws Exception{
-		Source src = new Source(source);
- 		if(!isPresentSource(src)){
+ 		if(!isPresentSource(source)){
 			throw new Exception("No city Named "+source);
 		}
-
-		if(!isPresentDestination(destination)){
+		if(!isPresentDestination(source,destination)){
 			throw new Exception("No city Named "+destination);
 		}
-
-		for(Source s : db.keySet()){
-			if(s.toString().equals(source) && db.get(s).toString().equals(destination))
-				return true;
-		}
+		if(db.get(source).contains(destination))
+			return true;
 		return false;
 	}
 
-	public static boolean isPresentSource(Source src){
-		for(Source s : db.keySet()){
-			if(s.toString().equals(src.toString()))
-				return true;
-		}
-		return false;
+	public static boolean isPresentSource(String src){
+		return (db.containsKey(src)) ? true : false;
 	}
 
-	public static boolean isPresentDestination(String dst){
-		for(Source s : db.keySet()){
-			if(db.get(s).toString().equals(dst))
+	public static boolean isPresentDestination(String source,String dst){ 
+		for(String s : db.keySet()){
+			if(db.get(s).contains(dst))
 				return true;
 		}
 		return false;
