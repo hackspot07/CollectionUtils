f41bb4339f8c39462c6c69e37a8e23893c914385
parmatma <pyadav.it@gmail.com>
2015-03-03 09:32:27 +0530

    version 2 started

--- PathTest.java
+++ PathTest.java
@@ -6,18 +6,18 @@ import static org.junit.Assert.*;
 public class PathTest{
 	@Test 
 	public void pathWillReturnTrue()throws Exception{
-		boolean getPathStatus  = Directpath.path("Bangalore","Singapore");
+		boolean getPathStatus  = Path.path("Bangalore","Singapore");
 		assertEquals(getPathStatus,true);
 	}
 	@Test 
 	public void pathWillReturnFalse()throws Exception{
-		boolean getPathStatus  = Directpath.path("Bangalore","Seoul");
+		boolean getPathStatus  = Path.path("Bangalore","Seoul");
 		assertEquals(getPathStatus,false);
 	}
 	@Test 
 	public void pathWillReturnNocityAsGivenSource()throws Exception{
 		try{
-			Directpath.path("Mumbai","Seoul");
+			Path.path("Mumbai","Seoul");
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 			assertEquals(e.getMessage(), "No city Named Mumbai");
@@ -26,24 +26,24 @@ public class PathTest{
 	@Test 
 	public void pathWillReturnNocityAsGivenDestination()throws Exception{
 		try{
-			Directpath.path("Bangalore","Mumbai");
+			Path.path("Bangalore","Mumbai");
 		}catch(Exception e){
 			assertEquals(e.getMessage(), "No city Named Mumbai");
 		}
 	}
 	@Test 
 	public void pathWillReturntruewForSingaporeToDubai()throws Exception{
-		boolean getPathStatus  = Directpath.path("Singapore","Dubai");
+		boolean getPathStatus  = Path.path("Singapore","Dubai");
 		assertEquals(getPathStatus,true);
 	}
 	@Test 
 	public void pathWillReturntruewForSingaporeToSeoul()throws Exception{
-		boolean getPathStatus  = Directpath.path("Singapore","Seoul");
+		boolean getPathStatus  = Path.path("Singapore","Seoul");
 		assertEquals(getPathStatus,true);
 	}
-	// @Test 
-	// public void pathWillReturnTrueFromBangaloretoTokyo()throws Exception{
-	// 	boolean getPathStatus  = Directpath.path("Bangalore","Tokyo");
-	// 	assertEquals(getPathStatus,"true");
-	// }
+	@Test 
+	public void pathWillReturnTrueFromBangaloretoTokyo()throws Exception{
+		boolean getPathStatus  = Path.path("Bangalore","Tokyo");
+		assertEquals(getPathStatus,"true");
+	}
 };
--- paths.java
+++ paths.java
@@ -2,7 +2,7 @@ class paths{
 	public static void main(String[] args)throws Exception{
 		boolean getStatus;
 		try{ 
-		 	getStatus = Directpath.path(args[0],args[1]);
+		 	getStatus = Path.path(args[0],args[1]);
 			System.out.println(getStatus);
 		}catch(Exception e){
 			System.out.println(e.getMessage());
